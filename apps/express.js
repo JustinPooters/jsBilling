beb68a3d7b854d75138a08a9136fe80b8762c06a
JustinPooters <48656828+JustinPooters@users.noreply.github.com>
2021-09-07 15:59:49 +0200

    Setup basic express & add information

--- apps/express.js
+++ apps/express.js
@@ -1,15 +1,26 @@
 function start() {
     var express = require('express');
+    var info = require("../about.json");
     // Create the app (Express)
     const app = express();
-
     // Set the port, i chose 1001 because why not (its the def web port)
     const port = 1001;
+    app.set('view engine', 'ejs');
 
     // Create a basic route for Hello world
     app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
+            var businessname = info.name;
+            var tagline = info.tagline;
+            var email = info.email;
+            var phone = info.phone;
+            var website = info.website;
+    
+        res.render('pages/index', {
+            name: businessname,
+            tagline: tagline
+        });
+      });
+      
 
     // Set the app and log the "i'm alive" to the console
     app.listen(port, () => {
