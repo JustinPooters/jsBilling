d7aa9f167cccf5b513e895046e6c1c72b4dabec3
JustinPooters <48656828+JustinPooters@users.noreply.github.com>
2021-09-07 17:30:25 +0200

    Fucked up the CSS files ^-^

--- apps/express.js
+++ apps/express.js
@@ -1,33 +1,47 @@
 function start() {
-    var express = require('express');
-    var info = require("../about.json");
-    // Create the app (Express)
+    const express = require('express');
+    const fs = require('fs')
+
+    let info = require("../about.json");
     const app = express();
-    // Set the port, i chose 1001 because why not (its the def web port)
     const port = 1001;
     app.set('view engine', 'ejs');
+    app.use(express.static(__dirname + '/public'));
 
-    // Create a basic route for Hello world
-    app.get('/', (req, res) => {
-            var businessname = info.name;
-            var tagline = info.tagline;
-            var email = info.email;
-            var phone = info.phone;
-            var website = info.website;
-    
-        res.render('pages/index', {
-            name: businessname,
-            tagline: tagline
+    const path = './config.env'
+    if (fs.existsSync(path)) {
+
+        app.get('/', (req, res) => {
+            let businessname = info.name;
+            let tagline = info.tagline;
+            let email = info.email;
+            let phone = info.phone;
+            let website = info.website;
+
+            res.render('pages/index', {
+                name: businessname,
+                tagline: tagline
+            });
         });
-      });
-      
 
-    // Set the app and log the "i'm alive" to the console
-    app.listen(port, () => {
-        console.log(`========================================================`)
-        console.log(`jsBilling started on: http://localhost:${port}`);
-        console.log(`========================================================`)
-    });
+
+        app.listen(port, () => {
+            console.log(`========================================================`)
+            console.log(`jsBilling started on: http://localhost:${port}`);
+            console.log(`========================================================`)
+        });
+    } else {
+
+        app.get('/', (req, res) => {
+            let businessname = 'jsBilling';
+
+            res.render('pages/setup', {
+                name: "jsBilling",
+                tagline: "jsBilling setup"
+            });
+        });
+
+    }
 }
 
 module.exports = {
